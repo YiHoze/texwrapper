d3db76aa384ad9344316e77fd284ef2dd878256a
yihoze <yihoze@icloud.com>
2018-04-16 20:22:35 +0900

    2018-04-16_20:22

--- makealbum.py
+++ makealbum.py
@@ -87,7 +87,7 @@ if args.hide_filename:
 with open(tex, mode='w') as f:
     f.write(content)
 
-os.system('powershell -command xelatex %s' %(tex))
+os.system('powershell -command lualatex %s' %(tex))
 os.system('texclean.py')
 os.system('powershell -command open.py %s' %(pdf))
 if not args.keep:
