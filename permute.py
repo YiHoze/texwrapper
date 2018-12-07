db05a9c8d9169b9318668eef674fe620e5fa47b9
yihoze <yihoze@icloud.com>
2018-12-07 16:41:30 +0900

    2018-12-07_16:41

--- permute.py
+++ permute.py
@@ -7,32 +7,61 @@ parser.add_argument(
     'letters',
     type = str,
     nargs = 1,
-    help = 'Type letters with space.'
+    help = 'Type letters.'
 )
 parser.add_argument(
-    '-c',
-    dest = 'count',
+    '-r',
+    dest = 'row',
     action = 'store_true',
     default = False,
-    help = 'Count the number of mutations.'
+    help = 'Show results in a row.'
+)
+parser.add_argument(
+    '-n',
+    dest = 'number',
+    action = 'store_true',
+    default = False,
+    help = 'Show results with number.'
+)
+parser.add_argument(
+    '-s',
+    dest = 'sort',
+    action = 'store_true',
+    default = False,
+    help = 'Sort letters before permuting.'
 )
 args = parser.parse_args()
-letters = args.letters[0].split(" ")
+# letters = args.letters[0].split(" ")
+letters = list(args.letters[0])
 
-cnt = 0
-def perm(a, k=0):    
-    global cnt
+num = 0
+row = []
+
+def permute(a, k=0):    
+    global num
+    global row
     if k == len(a):
-        str = ''.join(a)
-        cnt = cnt + 1
-        if args.count:
-            print(str, cnt)
+        result = ''.join(a)
+        if args.row:
+            if args.number:
+                num = num + 1                
+                row.append('[%d]%s' %(num, result))
+            else:
+                row.append(result)
         else:
-            print(str)
+            if args.number:
+                num = num + 1
+                print('%6d: %s' %(num, result))
+            else:
+                print(result)
     else:        
         for i in range(k, len(a)):            
             a[k], a[i] = a[i] ,a[k]
-            perm(a, k+1)
+            permute(a, k+1)
             a[k], a[i] = a[i], a[k]
 
-perm(letters)
+if args.sort:
+    letters.sort()
+permute(letters)
+if args.row:
+    print(' '.join(row))
