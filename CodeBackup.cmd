319f12798141184574be28d6b33351fe91d1ae66
yihoze <yihoze@icloud.com>
2019-03-14 10:59:31 +0900

    2019-03-14_10:59

---
+++ CodeBackup.cmd
@@ -0,0 +1,4 @@
+copy C:\Users\Hoze\AppData\Roaming\Code\User\snippets\latex.json c:\home\bin\misc\vscode /y
+copy C:\Users\Hoze\AppData\Roaming\Code\User\snippets\latex-expl3.json c:\home\bin\misc\vscode /y
+copy C:\Users\Hoze\AppData\Roaming\Code\User\keybindings.json c:\home\bin\misc\vscode /y
+copy C:\Users\Hoze\AppData\Roaming\Code\User\settings.json c:\home\bin\misc\vscode /y
