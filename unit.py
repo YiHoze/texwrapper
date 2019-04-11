a194655f24a4df3d054430298689a5c89b400053
yihoze <yihoze@icloud.com>
2019-04-11 16:43:07 +0900

    unit.py: a unit converter

--- unit.py
+++ unit.py
@@ -1,7 +1,7 @@
 import argparse
 
 parser = argparse.ArgumentParser(
-    description = 'Convert a unit to an another.'
+    description = 'Convert a unit to another.'
 )
 
 parser.add_argument(
@@ -20,49 +20,49 @@ parser.add_argument(
     dest = 'mile',
     action = 'store_true',
     default = False,
-    help = 'kilometer / mile'
+    help = 'mile / kilometer'
 )
 parser.add_argument(
     '-y',
     dest = 'yard',
     action = 'store_true',
     default = False,
-    help = 'meter / yard'
+    help = 'yard / meter'
 )
 parser.add_argument(
     '-f',
     dest = 'foot',
     action = 'store_true',
     default = False,
-    help = 'meter / foot'
+    help = 'foot / meter'
 )
 parser.add_argument(
     '-i',
     dest = 'inch',
     action = 'store_true',
     default = False,
-    help = 'millimeter / inch'
+    help = 'inch / millimeter'
 )
 parser.add_argument(
     '-pt',
     dest = 'point',
     action = 'store_true',
     default = False,
-    help = 'millimeter / point'
+    help = 'point / millimeter'
 )
 parser.add_argument(
     '-p',
     dest = 'pound',
     action = 'store_true',
     default = False,
-    help = 'kilogram / pound'
+    help = 'pound / kilogram'
 )
 parser.add_argument(
     '-py',
     dest = 'pyeong',
     action = 'store_true',
     default = False,
-    help = 'pyeong / square meter'
+    help = 'square meter / pyeong'
 )
 
 args = parser.parse_args()
@@ -70,68 +70,68 @@ args = parser.parse_args()
 def celsius_fahrenheit(temperature): 
     fahrenheit = float(temperature)
     celsius = (fahrenheit - 32) * 5 / 9
-    print('\n%3.2f Fahrenheit > %3.2f Celsius' %(fahrenheit, celsius))
+    print('\n%3.2f Fahrenheit = %3.2f Celsius' %(fahrenheit, celsius))
     celsius = float(temperature)
     fahrenheit = celsius * 9 / 5 + 32
-    print('%3.2f Celsius > %3.2f Fahrenheit\n' %(celsius, fahrenheit))
+    print('%3.2f Celsius = %3.2f Fahrenheit\n' %(celsius, fahrenheit))
 
 def kilometer_mile(distance): 
     mile = float(distance)
     kilometer = mile * 1.60934
-    print('\n%3.2f miles > %3.2f kilometers' %(mile, kilometer))
+    print('\n%3.2f miles = %3.2f kilometers' %(mile, kilometer))
     kilometer = float(distance)
     mile = kilometer * 0.621371
-    print('%3.2f kilometers > %3.2f miles\n' %(kilometer, mile))
+    print('%3.2f kilometers = %3.2f miles\n' %(kilometer, mile))
 
 def meter_yard(distance): 
     yard = float(distance)
     meter = yard * 0.9144
-    print('\n%3.2f yards > %3.2f meters' %(yard, meter))
+    print('\n%3.2f yards = %3.2f meters' %(yard, meter))
     meter = float(distance)
     yard = meter * 1.09361
-    print('%3.2f meters > %3.2f yards\n' %(meter, yard))
+    print('%3.2f meters = %3.2f yards\n' %(meter, yard))
 
 def meter_foot(distance): 
     foot = float(distance)
     meter = foot * 0.3048
-    print('\n%3.2f feet > %3.2f meters' %(foot, meter))
+    print('\n%3.2f feet = %3.2f meters' %(foot, meter))
     meter = float(distance)
     foot = meter * 3.28084
-    print('%3.2f meters > %3.2f feet\n' %(meter, foot))
+    print('%3.2f meters = %3.2f feet\n' %(meter, foot))
 
 def millimeter_inch(distance): 
     inch = float(distance)
     millimeter = inch * 25.4
     centimeter = millimeter / 10
-    print('\n%3.2f inches > %3.2f millmeters (%3.2f centimeters)' %(inch, millimeter, centimeter))
+    print('\n%3.2f inches = %3.2f millmeters (%3.2f centimeters)' %(inch, millimeter, centimeter))
     millimeter = float(distance)
     centimeter = millimeter / 10
     inch = millimeter * 0.0393701
-    print('%3.2f millimeters (%3.2f centimeters) > %3.2f inches\n' %(millimeter, centimeter, inch))
+    print('%3.2f millimeters (%3.2f centimeters) = %3.2f inches\n' %(millimeter, centimeter, inch))
 
 def millimeter_point(distance): 
     point = float(distance)
     millimeter = point * 0.352778
-    print('\n%3.2f points > %3.2f millmeters' %(point, millimeter))
+    print('\n%3.2f points = %3.2f millmeters' %(point, millimeter))
     millimeter = float(distance)
     point = millimeter * 2.83465
-    print('%3.2f millimeters > %3.2f points\n' %(millimeter, point))
+    print('%3.2f millimeters = %3.2f points\n' %(millimeter, point))
 
 def kilogram_pound(mass): 
     pound = float(mass)
     kilogram = pound * 2.20462
-    print('\n%3.2f pounds > %3.2f kilograms' %(pound, kilogram))
+    print('\n%3.2f pounds = %3.2f kilograms' %(pound, kilogram))
     kilogram = float(mass)
     pound = kilogram * 0.453592
-    print('%3.2f kilograms > %3.2f pounds\n' %(kilogram, pound))
+    print('%3.2f kilograms = %3.2f pounds\n' %(kilogram, pound))
 
 def kilogram_pound(area): 
     pyeong = float(area)
     squaremeter = pyeong * 3.30579
-    print('\n%3.2f pyeongs > %3.2f square meters' %(pyeong, squaremeter))
+    print('\n%3.2f pyeongs = %3.2f square meters' %(pyeong, squaremeter))
     squaremeter = float(area)
     pyeong = squaremeter * 0.3025
-    print('%3.2f square meters > %3.2f pyeongs\n' %(squaremeter, pyeong))
+    print('%3.2f square meters = %3.2f pyeongs\n' %(squaremeter, pyeong))
 
 if args.temperature: 
     celsius_fahrenheit(args.numeral)
